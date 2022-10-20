f014d34827493bd41ccc2ae1f948aff59ec97227
onkar <onkarvasav@gmail.com>
2022-10-20 17:44:55 +0530

    Font Added

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,74 +1,53 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 import { Box } from "@mui/system";
-import '../../overview.svg'
+// import '../../overview.svg'
 import "./sidebar.css";
 import Interview from "../Interview/Interview";
 
-
-
 const Sidebar = () => {
+  const [show, setShow] = useState(false);
 
-  const [show, setShow] = useState(false)
-  
   return (
     <Box className="headerss">
-  <nav>
-
+      <nav>
         <div className="overview font-nav">
-    <br />
-  
-    <a href="javascript:location.reload(true)">
-       <img  src="Overview.svg" alt="" /></a>
-        
-        Overview
-        
-       
+          <a href="javascript:location.reload(true)">
+            <img src="Ov.svg" alt="" />
+          </a>
+          Overview
         </div>
-
-
 
         <div className="Calender">
+          {show ? <Interview /> : null}
 
-        {
-
-show ?      <Interview /> : null
-
-}
-
-          <img 
-         onClick={()=>setShow(true)}
-          src="Calender.svg" alt="" />
+          <img onClick={() => setShow(true)} src="Calender.svg" alt="" />
         </div>
-   
-        <div className="Notes "><img src="Notes.svg" alt="" /></div>
-        <div className="Tasks "><img src="Tasks.svg" alt="" /></div>
-    
-</nav>
+
+        <div className="Notes ">
+          <img src="Notes.svg" alt="" />
+        </div>
+        <div className="Tasks ">
+          <img src="Tasks.svg" alt="" />
+        </div>
+      </nav>
       <div>
         <div className="Invite">
-        <br /> 
-      <img src="Invite.svg" alt="" />
-     
-        Invite</div>
+          <br />
+          <img src="Invite.svg" alt="" />
+          Invite
+        </div>
         <div className="Chat">
-        <img src="Chat.svg" alt="" />
-        Chat</div>
+          <img src="Chat.svg" alt="" />
+          Chat
+        </div>
         <div className="">
-        <img src="Arrow.svg" alt="" />
+          <img src="Arrow.svg" alt="" />
         </div>
         <div className="AP">
-        <img src="Avatar.svg" alt="" />
+          <img src="Avatar.svg" alt="" />
         </div>
-
       </div>
-
-
-
-     
-
-
-
     </Box>
   );
 };
